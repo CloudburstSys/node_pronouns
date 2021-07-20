be022a0cdbded5bfd67cb43d701c9ad2c8a9b027
LegoDev <me@legodev.com>
2021-07-20 11:03:09 +0100

    fixing some things

--- index.js
+++ index.js
@@ -4,35 +4,43 @@ var pronouns = require("./resources/pronouns.json");
 
 const stringModifier = new StringModifier(pronouns);
 
-module.exports = (string, data) => {
-	if(Array.isArray(data)) {
+module.exports = (string, data, ignoreFile = false) => {
+	if (Array.isArray(data)) {
 		// multiple
-		return multiple(data)(string);
+		return multiple(data, ignoreFile)(string);
 	} else {
 		// single
-		return single(data)(string);
+		return single(data, ignoreFile)(string);
 	}
 }
 
-function single(pronoun) {
+function single(pronoun, ignoreFile = false) {
 	this.pronoun = null;
 
-	pronouns.forEach(pronounSet => {
-		if(pronounSet.join("/").startsWith(pronoun)) this.pronoun = pronounSet;
-	});
-	
+	if (!ignoreFile) {
+		pronouns.forEach(pronounSet => {
+			if (pronounSet.join("/").startsWith(pronoun.join("/"))) this.pronoun = pronounSet;
+		});
+	} else {
+		this.pronoun = pronoun;
+	}
+
 	return (string) => {
 		return stringModifier.modifySingle(string, this.pronoun);
 	}
 }
 
-function multiple(pronounArray) {	
+function multiple(pronounArray, ignoreFile = false) {
 	this.pronouns = [];
-	
+
 	pronounArray.forEach(pronoun => {
-		pronouns.forEach(pronounSet => {
-			if(pronounSet.join("/").startsWith(pronoun)) this.pronouns.push(pronounSet);
-		})
+		if (!ignoreFile) {
+			pronouns.forEach(pronounSet => {
+				if (pronounSet.join("/").startsWith(pronoun.join("/"))) this.pronouns.push(pronounSet);
+			});
+		} else {
+			this.pronouns.push(pronoun);
+		}
 	});
 
 	return (string) => {
@@ -41,15 +49,15 @@ function multiple(pronounArray) {
 }
 
 module.exports.extendPronouns = (pronounArray) => {
-	if(!Array.isArray(pronounArray)) throw new Error("PronounArray MUST be an array!");
-	if(!Array.isArray(pronounArray[0])) throw new Error("PronounArray must contain arrays");
-	
+	if (!Array.isArray(pronounArray)) throw new Error("PronounArray MUST be an array!");
+	if (!Array.isArray(pronounArray[0])) throw new Error("PronounArray must contain arrays");
+
 	pronounArray.forEach(pronoun => {
 		pronouns.push(pronoun);
 	});
 
 	stringModifier.addPronouns(pronounArray);
-	
+
 	return true;
 }
 
